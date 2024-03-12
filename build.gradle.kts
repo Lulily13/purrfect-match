936e31e2dc37d8c7674927e927cc5058568de171
Lulily13 <bloodyakuma13@yahoo.com>
2024-03-12 15:53:08 +0100

    spring boot setup

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "io.github.lulily13"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    withType<Wrapper> {
+        gradleVersion = "8.6"
+        distributionType = Wrapper.DistributionType.ALL
+    }
+
+    withType<KotlinCompile> {
+        incremental = true
+        kotlinOptions {
+            jvmTarget = "21"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+}
