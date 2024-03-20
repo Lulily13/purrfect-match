1b9707014a99e22bb790d16109d4581f17c4e224
Lulily13 <bloodyakuma13@yahoo.com>
2024-03-20 15:41:06 +0100

    created application controllers

--- src/main/kotlin/io/github/lulily13/purrfectmatch/controller/ContactController.kt
+++ src/main/kotlin/io/github/lulily13/purrfectmatch/controller/ContactController.kt
@@ -1,6 +1,5 @@
 package io.github.lulily13.purrfectmatch.controller
 
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
